4e1bf33c0305985e917c81ad4f5b0eb4ef3ee726
rafaela-alexandre <rafaelaoa@al.insper.edu.br>
2026-04-06 12:53:06 -0300

    roteiro 1.0.1

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 [![Compilation Status](https://compiler-tester.insper-comp.com.br/svg/rafaela-alexandre/log-comp)](https://compiler-tester.insper-comp.com.br/svg/rafaela-alexandre/log-comp)
 
 This repository is monitored by Compiler Tester for automatic compilation status.
+## Diagrama Sintático
+
+![Diagrama Sintático](diagrama.png)
---
+++ roteiro1.py
@@ -0,0 +1,61 @@
+import sys
+
+
+def main():
+    entrada =  sys.argv[1]
+    resultado = processar(entrada)
+    print(resultado)
+
+
+def processar(texto):
+    texto = texto.strip()
+
+    if texto == "":
+        raise Exception("entrada invalida")
+
+    numero_atual = ""
+    resultado = 0
+    sinal = "+"
+    ultimo = "inicio"
+
+    for i in texto:
+        if i == " ":
+            if numero_atual != "":
+                ultimo = "numero_espacado"
+            continue
+
+        elif i.isdigit():
+            if ultimo == "numero_espacado":
+                raise Exception("entrada invalida")
+            numero_atual += i
+            ultimo = "numero"
+
+        elif i == "+" or i == "-":
+            if numero_atual == "":
+                raise Exception("entrada invalida")
+
+            if sinal == "+":
+                resultado += int(numero_atual)
+            else:
+                resultado -= int(numero_atual)
+
+            numero_atual = ""
+            sinal = i
+            ultimo = "operador"
+
+        else:
+            raise Exception("entrada invalida")
+
+    if numero_atual == "":
+        raise Exception("entrada invalida")
+
+    if sinal == "+":
+        resultado += int(numero_atual)
+    else:
+        resultado -= int(numero_atual)
+
+    return resultado
+
+
+if __name__ == "__main__":
+    main()
