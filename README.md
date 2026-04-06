4d8bccaa35ef6b0039c464c3fb20df82df7c6d3b
rafaela-alexandre <rafaelaoa@al.insper.edu.br>
2026-04-06 13:59:47 -0300

    Roteiro 5 v2.0.0

--- README.md
+++ README.md
@@ -4,13 +4,17 @@
 
 This repository is monitored by Compiler Tester for automatic compilation status.
 ## Diagrama Sintático
-![alt text](image.png)
+![alt text](image-1.png)
 
-#EBNF
+## EBNF
 ```ebnf
+PROGRAM = { STATEMENT } ;
+STATEMENT = ((IDENTIFIER, "=", EXPRESSION) | (PRINT, "(", EXPRESSION, ")") | ε), EOL ;
 EXPRESSION = TERM, { ("+" | "-"), TERM } ;
 TERM = FACTOR, { ("*" | "/"), FACTOR } ;
 FACTOR = ("+" | "-"), FACTOR | "(", EXPRESSION, ")" | NUMBER ;
 NUMBER = DIGIT, {DIGIT} ;
 DIGIT = 0 | 1 | ... | 9 ;
+IDENTIFIER = LETTER, {LETTER | DIGIT | "_"} ;
+LETTER = a | b | ... | z | A | B | ... | Z ;
 ```
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 
 class Token:
@@ -7,11 +8,44 @@ class Token:
         self.value = value
 
 
+class PrePro:
+    def filter(code: str) -> str:
+        return re.sub(r'--[^\n]*', '', code)
+
+
+class Variable:
+    def __init__(self, value):
+        self.value = value
+
+
+class SymbolTable:
+    def __init__(self):
+        self._table = {}
+
+    @property
+    def table(self):
+        return self._table
+
+    @table.setter
+    def table(self, value):
+        self._table = value
+
+    def get(self, name: str):
+        if name not in self._table:
+            raise Exception(f"[Semantic] Variable '{name}' not defined")
+        return self._table[name].value
+
+    def set(self, name: str, value):
+        self._table[name] = Variable(value)
+
+
 class Lexer:
     def __init__(self, source: str):
         self.source = source
         self.position = 0
         self.next = None
+
+    RESERVED = {"print"}
 
     def select_next(self):
         while self.position < len(self.source) and self.source[self.position] == " ":
@@ -23,7 +57,10 @@ class Lexer:
 
         char = self.source[self.position]
 
-        if char == "+":
+        if char == "\n":
+            self.next = Token("END", "\n")
+            self.position += 1
+        elif char == "+":
             self.next = Token("PLUS", "+")
             self.position += 1
         elif char == "-":
@@ -41,12 +78,24 @@ class Lexer:
         elif char == ")":
             self.next = Token("CLOSE_PAR", ")")
             self.position += 1
+        elif char == "=":
+            self.next = Token("ASSIGN", "=")
+            self.position += 1
         elif char.isdigit():
             num = ""
             while self.position < len(self.source) and self.source[self.position].isdigit():
                 num += self.source[self.position]
                 self.position += 1
             self.next = Token("INT", int(num))
+        elif char.isalpha():
+            iden = ""
+            while self.position < len(self.source) and (self.source[self.position].isalnum() or self.source[self.position] == "_"):
+                iden += self.source[self.position]
+                self.position += 1
+            if iden in Lexer.RESERVED:
+                self.next = Token(iden.upper(), iden)
+            else:
+                self.next = Token("IDEN", iden)
         else:
             raise Exception(f"[Lexer] Invalid symbol '{char}'")
 
@@ -56,7 +105,7 @@ class Node:
         self.value = value
         self.children = children if children is not None else []
 
-    def evaluate(self):
+    def evaluate(self, st):
         raise NotImplementedError("[Semantic] evaluate() not implemented")
 
 
@@ -64,7 +113,7 @@ class IntVal(Node):
     def __init__(self, value, children):
         super().__init__(value, children)
 
-    def evaluate(self):
+    def evaluate(self, st):
         return self.value
 
 
@@ -72,11 +121,11 @@ class UnOp(Node):
     def __init__(self, value, children):
         super().__init__(value, children)
 
-    def evaluate(self):
+    def evaluate(self, st):
         if self.value == "+":
-            return +self.children[0].evaluate()
+            return +self.children[0].evaluate(st)
         elif self.value == "-":
-            return -self.children[0].evaluate()
+            return -self.children[0].evaluate(st)
         else:
             raise Exception(f"[Semantic] Unknown unary operator '{self.value}'")
 
@@ -85,9 +134,9 @@ class BinOp(Node):
     def __init__(self, value, children):
         super().__init__(value, children)
 
-    def evaluate(self):
-        left = self.children[0].evaluate()
-        right = self.children[1].evaluate()
+    def evaluate(self, st):
+        left = self.children[0].evaluate(st)
+        right = self.children[1].evaluate(st)
         if self.value == "+":
             return left + right
         elif self.value == "-":
@@ -100,6 +149,47 @@ class BinOp(Node):
             return left // right
         else:
             raise Exception(f"[Semantic] Unknown binary operator '{self.value}'")
+
+
+class Identifier(Node):
+    def __init__(self, value, children):
+        super().__init__(value, children)
+
+    def evaluate(self, st):
+        return st.get(self.value)
+
+
+class Assignment(Node):
+    def __init__(self, value, children):
+        super().__init__(value, children)
+
+    def evaluate(self, st):
+        st.set(self.children[0].value, self.children[1].evaluate(st))
+
+
+class Print(Node):
+    def __init__(self, value, children):
+        super().__init__(value, children)
+
+    def evaluate(self, st):
+        print(self.children[0].evaluate(st))
+
+
+class Block(Node):
+    def __init__(self, value, children):
+        super().__init__(value, children)
+
+    def evaluate(self, st):
+        for child in self.children:
+            child.evaluate(st)
+
+
+class NoOp(Node):
+    def __init__(self, value, children):
+        super().__init__(value, children)
+
+    def evaluate(self, st):
+        pass
 
 
 class Parser:
@@ -127,6 +217,11 @@ class Parser:
             Parser.lexer.select_next()
             return node
 
+        elif Parser.lexer.next.type == "IDEN":
+            node = Identifier(Parser.lexer.next.value, [])
+            Parser.lexer.select_next()
+            return node
+
         else:
             raise Exception(f"[Parser] Unexpected token {Parser.lexer.next.type}, expected factor")
 
@@ -150,11 +245,54 @@ class Parser:
 
         return node
 
+    def parse_statement() -> Node:
+        # linha vazia
+        if Parser.lexer.next.type == "END":
+            Parser.lexer.select_next()
+            return NoOp(None, [])
+
+        # atribuição: IDEN = EXPRESSION
+        elif Parser.lexer.next.type == "IDEN":
+            iden = Identifier(Parser.lexer.next.value, [])
+            Parser.lexer.select_next()
+            if Parser.lexer.next.type != "ASSIGN":
+                raise Exception(f"[Parser] Expected '=' but got {Parser.lexer.next.type}")
+            Parser.lexer.select_next()
+            expr = Parser.parse_expression()
+            if Parser.lexer.next.type != "END":
+                raise Exception(f"[Parser] Expected newline but got {Parser.lexer.next.type}")
+            Parser.lexer.select_next()
+            return Assignment(None, [iden, expr])
+
+        # print(EXPRESSION)
+        elif Parser.lexer.next.type == "PRINT":
+            Parser.lexer.select_next()
+            if Parser.lexer.next.type != "OPEN_PAR":
+                raise Exception(f"[Parser] Expected '(' but got {Parser.lexer.next.type}")
+            Parser.lexer.select_next()
+            expr = Parser.parse_expression()
+            if Parser.lexer.next.type != "CLOSE_PAR":
+                raise Exception(f"[Parser] Expected ')' but got {Parser.lexer.next.type}")
+            Parser.lexer.select_next()
+            if Parser.lexer.next.type != "END":
+                raise Exception(f"[Parser] Expected newline but got {Parser.lexer.next.type}")
+            Parser.lexer.select_next()
+            return Print(None, [expr])
+
+        else:
+            raise Exception(f"[Parser] Unexpected token {Parser.lexer.next.type} in statement")
+
+    def parse_program() -> Node:
+        children = []
+        while Parser.lexer.next.type != "EOF":
+            children.append(Parser.parse_statement())
+        return Block(None, children)
+
     def run(code: str) -> Node:
         Parser.lexer = Lexer(code)
         Parser.lexer.select_next()
 
-        node = Parser.parse_expression()
+        node = Parser.parse_program()
 
         if Parser.lexer.next.type != "EOF":
             raise Exception(f"[Parser] Unexpected token {Parser.lexer.next.type}, expected EOF")
@@ -163,9 +301,13 @@ class Parser:
 
 
 def main():
-    code = sys.argv[1]
-    result = Parser.run(code).evaluate()
-    print(result)
+    filename = sys.argv[1]
+    with open(filename, "r") as f:
+        code = f.read() + "\n"
+
+    code = PrePro.filter(code)
+    st = SymbolTable()
+    Parser.run(code).evaluate(st)
 
 
 if __name__ == "__main__":
